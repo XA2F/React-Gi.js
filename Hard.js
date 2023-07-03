4ce186ecc5a8a17f895c89017f75812d462e9457
xa2f <Abidjanyapo@gmail.com>
2023-07-03 11:38:29 -0400

    updated

---
+++ Hard.js
@@ -0,0 +1,36 @@
+// contactsData.js
+
+const contactsData = [
+  {
+    name: " David Nangui",
+    number: " 704-809-9023",
+    DOB: " Jun 21, 2004",
+    height: ' 5"10',
+    weight: " 180",
+  },
+  {
+    name: " lam Nangu",
+    number: " 704-809-9013",
+    DOB: " Jun 21, 2009",
+    height: ' 5"0',
+    weight: " 120",
+  },
+];
+
+export default contactsData;
+
+// App.js
+
+// function app() {
+//   const [contacts, setContacts] = useState(contactsData);
+
+//   return (
+//     <div>
+//       {/* Render the list of contacts */}
+//     </div>
+//   );
+// }
+
+// export default app;
+
+// App.js (continued)
